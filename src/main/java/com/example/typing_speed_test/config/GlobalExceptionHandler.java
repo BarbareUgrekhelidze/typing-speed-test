b95f304a0bdd40a9fea5e30c5caed637daac502f
BarbareUgrekhelidze <barbareugrekhelidze32@gmail.com>
2026-08-28 18:36:38 +0400

    added global exception handler

---
+++ src/main/java/com/example/typing_speed_test/config/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.typing_speed_test.config;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+}
