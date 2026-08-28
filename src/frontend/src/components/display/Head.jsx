c2f3be3ca63453f8c2ac15a7bc4ebfe70eddfbbb
BarbareUgrekhelidze <barbareugrekhelidze32@gmail.com>
2026-08-28 22:43:25 +0400

    started personal best calculation

--- src/frontend/src/components/display/Head.jsx
+++ src/frontend/src/components/display/Head.jsx
@@ -1,12 +1,25 @@
 import './DisplayStyle.css'
+import {useState} from "react";
 
 function Head(){
+    const [personalBest, setPersonalBest] = useState(0)
+
+    function calculatePersonalBest(){
+        const name = localStorage.getItem('username')?.trim();
+
+        if (!name || name === "null" || name === "undefined") {
+            return;
+        }
+
+        {/* todo calculate personal best */}
+    }
+
     return (
         <div className="head">
             <img src="images/logo-light.png" alt=""/>
             <div className="personal-best">
                 <img src="images/icon-personal-best.svg" alt=""/>
-                <p>Personal Best: <span>0 WPM</span></p>
+                <p>Personal Best: <span>{`${personalBest} WPM`}</span></p>
             </div>
         </div>
     )
